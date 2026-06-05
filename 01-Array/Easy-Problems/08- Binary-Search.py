9926775271f5a64c50ef122e64e499fbdd70b5c4
umesh-yadav-777 <yaduvanshiumesh045@gmail.com>
2026-06-05 11:03:09 +0530

    Completed binary serach solution

--- 01-Array/Easy-Problems/08- Binary-Search.py
+++ 01-Array/Easy-Problems/08- Binary-Search.py
@@ -1,3 +1,23 @@
-# Works only on sorted arrays.
+def binary_search(arr, key):
+    start = 0
+    end = len(arr)-1
 
- 
+
+    while (start <= end):
+        mid = (start + end) // 2
+
+        # comparisons
+        if arr[mid] == key:  # Found
+            return mid
+
+        if arr[mid] < key:     # Update right half
+            start = mid + 1
+
+        else:                  # Update Left half
+            end = mid - 1
+
+    return -1
+
+arr = [2,4,6,8,10,12,14]
+key = 4
+print("Index for key is : ",binary_search(arr,key))
